7a21f2ef414790562ad49d28a91829e74af852b6
Cashigtro <cashignitro@gmail.com>
2023-11-04 15:30:20 +1100

    attempted

--- scripts/launcher.gd
+++ scripts/launcher.gd
@@ -1,7 +1,25 @@
 extends Area2D
 
+var arrow_notif = preload("res://scenes/arrow_notif.tscn")
+@onready var destination = $"/root/main/ui/scores/score_containers/pollution/pollution"
+@onready var child_location = $"/root/main/ui/notifs"
+var notif = false
+
 func _on_body_entered(body):
 	if body.name == "player":
 		body.velocity.y = -2200
+		
+		if notif:
+			var a = arrow_notif.instantiate()
+		
+			print(destination)
+			
+			a.position = body.position
+
+			child_location.add_child(a)
+			
+			var tween = create_tween()
+			tween.tween_property(a, "position", destination.position, 1)
+		
 		global.score += 3
 		global.arrow_score += 1
